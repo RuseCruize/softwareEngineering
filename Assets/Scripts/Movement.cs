da1888108f5e418133c4d88b4b7c806640252e49
Davies <UM-AD\mbddq7@rc14xcs213.managed.mst.edu>
2020-10-04 12:43:25 -0500

    Added a feature for our guy to jump. Additionally updated the version from 2019.3.0f6 to 2019.4.11f1

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -6,6 +6,13 @@ public class Movement : MonoBehaviour
 {
     public float speed;
     public Rigidbody2D body;
+    private BoxCollider2D boxCollider;
+    public LayerMask groundLayer;
+
+    private void Awake()
+    {
+        boxCollider = gameObject.GetComponent<BoxCollider2D> ();
+    }
 
     // Update is called once per frame
     void Update()
@@ -14,12 +21,20 @@ public class Movement : MonoBehaviour
         float leftSpeed = Input.GetKey(KeyCode.LeftArrow) ? -speed : 0;
         float rightSpeed = Input.GetKey(KeyCode.RightArrow) ? speed : 0;
 
-        if (isUp)
+        if (this.isGrounded() && isUp)
         {
             // Jump logic, check to see if touching ground
+            float jumpVelocity = 10;
+            body.velocity = Vector2.up * jumpVelocity;
         }
 
         transform.position += transform.right * leftSpeed * Time.deltaTime;
         transform.position += transform.right * rightSpeed * Time.deltaTime;
+    }
+
+    bool isGrounded()
+    {
+        RaycastHit2D raycastHit = Physics2D.Raycast(transform.position, Vector2.down, 1, groundLayer);
+        return raycastHit.collider != null;
     }
 }
