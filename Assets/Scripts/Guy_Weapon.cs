64cc9eb7aa59c406b6122f3ab2cb0bd984e343f9
Ethan <emad5n@umsystem.edu>
2020-10-04 12:50:06 -0500

    Current working status for adding a weapon manager with proof of concept weapon to prove it works as expected

---
+++ Assets/Scripts/Guy_Weapon.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(AudioSource))]
+
+public class Guy_Weapon : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Transform weaponTip;
+    public bool singleFire = true;
+    public float fireRate = 1.0f;
+    public GameObject bulletPrefab;
+    public Vector2 lookDirection;
+    public float lookAngle;
+    public int bulletsPerMagazine = 1;
+    public float weaponDamage = 30;
+    public float timeToReload = 0f;
+    public AudioClip fireAudio;
+
+    [HideInInspector]
+    public Guy_WeaponManager manager;
+
+    float nextFireTime = 0;
+    int bulletsPerMagazineDefault = 1;
+    AudioSource audioSource;
+    void Start()
+    {
+        bulletsPerMagazine = bulletsPerMagazineDefault;
+        audioSource = GetComponent<AudioSource>();
+        audioSource.playOnAwake = false;
+        audioSource.spatialBlend = 1f;        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (manager.Aiming) { 
+            lookDirection = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+            lookAngle = Mathf.Atan2(lookDirection.y, lookDirection.x) * Mathf.Rad2Deg;
+            transform.rotation = Quaternion.Euler(0f, 0f, lookAngle - 90f);
+        }
+        if (Input.GetMouseButtonDown(0)) {
+            Aim();
+        }
+        
+    }
+
+    void Aim() {
+        if (manager.canFire) {
+            manager.Aiming = true;
+            //figure out how to fire from here
+            if (Input.GetMouseButtonDown(0)) {
+                Fire();
+            }
+            manager.Aiming = false;
+            //after firing, reset aiming to false
+        }
+    }
+
+    void Fire() {
+        GameObject guyBullet = Instantiate(Guy_Bullet, weaponTip.position, weaponTip.rotation);
+        guyBullet.GetComponent<Rigidbody2D>().velocity = weaponTip.up * 10f;
+    }
+
+    public void ActivateWeapon(bool activate) {
+        StopAllCoroutines();
+        manager.canFire = true;
+        gameObject.SetActive(activate);
+    }
+
+    public void DeactivateWeapon(bool deactivate) {
+        
+    }
+}
---
+++ Assets/Scripts/Guy_WeaponManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Guy_WeaponManager : MonoBehaviour
+{
+    public bool Aiming = false;
+    public bool canFire = false;
+    public Guy_Weapon primaryWeapon;
+    public Guy_Weapon secondaryWeapon;
+
+    [HideInInspector]
+    public Guy_Weapon selectedWeapon;
+    // Start is called before the first frame update
+    void Start()
+    {
+        primaryWeapon.ActivateWeapon(true);
+        secondaryWeapon.ActivateWeapon(false);
+        selectedWeapon = primaryWeapon;
+        primaryWeapon.manager = this;
+        secondaryWeapon.manager = this;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
