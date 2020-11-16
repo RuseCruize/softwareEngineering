3a057fe89ddaa63ae367f4f1bda8ff339d7449b0
mpd4vb <mpd4vb@umsystem.edu>
2020-11-15 18:22:18 -0600

    added indicator for which guy is moving

---
+++ Assets/Scripts/PlayerTextHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerTextHandler : MonoBehaviour
+{
+    [SerializeField] GameObject indicator;
+    [SerializeField] MatchManager mm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //text.transform.position = mm.currentGuy.transform.position + new Vector3(0.0f, 2.0f, 0.0f);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        indicator.transform.position = mm.currentGuy.transform.position + new Vector3(0.0f, 2.0f, 0.0f);
+    }
+}
